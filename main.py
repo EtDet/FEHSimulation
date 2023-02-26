50769ca5cc88f367a47d50b9f2b0651f166233be
Ethan Dettmann <91626285+EtDet@users.noreply.github.com>
2023-02-25 19:19:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from future.moves.tkinter import *
+from tkinter import *
+from PIL import Image, ImageTk
 
 class Tile:
 
@@ -32,12 +33,37 @@ class Hero:
         self.move = movement  # 0 - inf, 1 - cav, 2 - fly, 3 - arm
 
 
+def draw():
+    root = Tk()
+    root.title('FEH Simulation Window')
+    root.iconbitmap("C:\\Users\\eman5\\PycharmProjects\\FEHSimulation\\Sprites\\marth.ico")
+    root.geometry("480x740")
+
+    cWidth = 480
+    cHeight = 640
+
+    myCanvas = Canvas(root, width=cWidth, height=cHeight, bg='white')
+    myCanvas.pack(pady=50)
+    i = 0
+
+    while i < 6:
+        j = 0
+        while j < 8:
+            myCanvas.create_rectangle(i * (cWidth / 6), j * (cHeight / 8), (i + 1) * (cWidth / 6),
+                                      (j + 1) * (cHeight / 8), fill="#4b9431")
+            j += 1
+        i += 1
+
+    marthSprite = Image.open('C:\\Users\\eman5\\PycharmProjects\\FEHSimulation\\Sprites\\marth.png')
+    resize = marthSprite.resize((cWidth // 6, cHeight // 8))
+    img = ImageTk.PhotoImage(resize)
+
+    myCanvas.create_image(0,0,anchor=NW,image=img)
+
+    root.mainloop()
+
 marth = Hero(41,31,34,29,23,"Sword",0)
 map = Map()
 map.placeHero(marth,7,2)
 
-root = Tk()
-root.title('FEH Simulation Window')
-root.iconbitmap('c:/Users/Ethan/PycharmProjects/FEHSimulation/Sprites/marth.png')
-root.mainLoop()
-#myCanvas = Canvas(root, width = 300, height = 400, bg = 'white')
+draw()
