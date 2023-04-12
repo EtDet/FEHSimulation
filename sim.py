6c284298a2f144832b430eac3695b509ca4a1d63
Ethan Dettmann <91626285+EtDet@users.noreply.github.com>
2023-04-12 17:08:57 -0400

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,17 +1,34 @@
 from combat import *
 from queue import Queue
+from copy import copy
 
 class Tile:
     def __init__(self,terrain,x,y):
 
+        # coordinate grid system starting at top left
         self.x = x
         self.y = y
 
+        # numbered tile system starting at bottom left, going right
+        self.tileNo = 0
+
         self.terrain = terrain
+        # 0 - normal
+        # 1 - forest
+        # 2 - flierOnly
+        # 3 - defensive
+        # 4 - trench
+        # 5 - defensive trench
+        # 6 - wall
+
+        # pointer to hero currently on this tile
         self.heroOn = None
+        # hero currently on this tile if using save skills
         self.saviorHeroOn = None
+        # no
         self.effects = None
 
+        # tile to each cardinal direction of each node
         self.north = None
         self.east = None
         self.south = None
@@ -21,55 +38,44 @@ class Tile:
     def getY(self): return self.y
     def getCoords(self): return self.x,self.y
 
-    def setNorth(self,north):
-        self.north = north
+    def setTileNo(self,tileNo): self.tileNo = tileNo
+    def getTileNo(self): return self.tileNo
 
-    def setEast(self,east):
-        self.east = east
+    def getNorth(self): return self.north
+    def getEast(self): return self.east
+    def getSouth(self): return self.south
+    def getWest(self): return self.west
 
-    def setSouth(self,south):
-        self.south = south
+    def setNorth(self,north): self.north = north
+    def setEast(self,east): self.east = east
+    def setSouth(self,south): self.south = south
+    def setWest(self,west): self.west = west
 
-    def setWest(self,west):
-        self.west = west
+    def getHero(self): return self.heroOn
+    def setHero(self,hero): self.heroOn = hero
 
-    def getNorth(self):
-        return self.north
-
-    def getEast(self):
-        return self.east
-
-    def getSouth(self):
-        return self.south
-
-    def getWest(self):
-        return self.west
-
-    def setHero(self,hero):
-        self.heroOn = hero
-
-    def getHero(self):
-        return self.heroOn
-
-    def getTerrain(self):
-        return self.terrain
+    def getTerrain(self): return self.terrain
+    def setTerrain(self,terrain): self.terrain = terrain
 
     def printHero(self):
         if self.heroOn == None: print("None")
         else: print(self.heroOn.getName())
 
     def getTilesWithinNSpaces(self,n):
-        fringe = Queue()
-        tilesWithin = []
-        fringe.put(self)
+        fringe = Queue() #tiles to visit
+        tilesWithin = [] #tiles visited
+        fringe.put(self) #put this node in fringe
         tilesWithin.append(self)
+
+        # Breadth First Search of local tiles within n spaces
 
         level = 0
         fringe.put(Tile(-1,-1,-1))
+
         while not fringe.empty() and level < n:
+
             cur = fringe.get()
 
-            #print(cur.getX(),cur.getY())
             if cur.getNorth() != None and cur.getNorth() not in tilesWithin:
                 fringe.put(cur.getNorth())
                 tilesWithin.append(cur.getNorth())
@@ -89,23 +95,26 @@ class Tile:
 
         return tilesWithin
 
-    def isWithinNTiles(self,other,n):
+    # see if otherHero is within n spaces of unit on this tile
+    def isOtherWithinNTiles(self,otherHero,n):
         tiles = self.getTilesWithinNSpaces(n)
         for tile in tiles:
-            if tile.getHero() == other:
+            if tile.getHero() == otherHero:
                 return True
         return False
 
-
+# subclass of tile unit can start on
 class StartingTile(Tile):
     def __init__(self,terrain,num,side,x,y):
         super().__init__(terrain,x,y)
         self.num = num
-        self.side = side
+        self.side = side # false - player, true - enemy
 
+# current map with units
 class Map:
     def __init__(self,stp,ste):
 
+        # generate 8x6 map of tiles of default terrain
         self.map=[]
         rows, cols = 8,6
         i = 0
@@ -118,8 +127,8 @@ class Map:
             self.map.append(col)
             i += 1
 
+        # list of two lists of starting tiles
         self.startingTiles = [[], []]
-
 
         i = 0
         while i < len(stp):
@@ -135,6 +144,7 @@ class Map:
             #self.currentTiles[1].append(self.map[ste[i][0]][ste[i][1]])
             i += 1
 
+        # set cardinal directions
         i = 0
         while i < len(self.map):
             j = 0
@@ -150,68 +160,109 @@ class Map:
                 j += 1
             i += 1
 
-
+    # place player units on player starting tiles, same for enemy
     def placeUnits(self,playerUnits,enemyUnits):
 
-        self.currentTiles = [{}, {}]
         self.playerUnits = playerUnits
         self.enemyUnits = enemyUnits
 
+        #place units
         i = 0
         while i < len(self.startingTiles[0]):
             self.startingTiles[0][i].setHero(playerUnits[i])
-            self.currentTiles[0].update({playerUnits[i].getName(): self.startingTiles[0][i]})
             playerUnits[i].setTile(self.startingTiles[0][i])
             i += 1
+
         i = 0
         while i < len(self.startingTiles[1]):
             self.startingTiles[1][i].setHero(enemyUnits[i])
-            self.currentTiles[1].update({enemyUnits[i].getName(): self.startingTiles[1][i]})
             enemyUnits[i].setTile(self.startingTiles[1][i])
             i += 1
 
-        #print(self.currentTiles)
+        #initialize starting stats w/ weapon might, skills
+        player = self.getNextUnits(False)
+        for x in player:
+            plrStats = x.getStats()
+            plrStats[1] += x.getWeapon().getMT()
+            plrSkills = x.getSkills()
+            for key in plrSkills:
+                if key == "HPBoost":  plrStats[0] += plrSkills["HPBoost"]
+                if key == "atkBoost": plrStats[1] += plrSkills["atkBoost"]
+                if key == "spdBoost": plrStats[2] += plrSkills["spdBoost"]
+                if key == "defBoost": plrStats[3] += plrSkills["defBoost"]
+                if key == "resBoost": plrStats[4] += plrSkills["resBoost"]
+            x.setStats(plrStats)
 
-    def moveManual(self,unit,newSpace):
-        return
 
-    def getMap(self):
-        return self.map
+        enemy = self.getNextUnits(True)
+        for x in enemy:
+            emyStats = x.getStats()
+            emyStats[1] += x.getWeapon().getMT()
+            emySkills = x.getSkills()
+            for key in emySkills:
+                if key == "HPBoost":  emyStats[0] += emySkills["HPBoost"]
+                if key == "atkBoost": emyStats[1] += emySkills["atkBoost"]
+                if key == "spdBoost": emyStats[2] += emySkills["spdBoost"]
+                if key == "defBoost": emyStats[3] += emySkills["defBoost"]
+                if key == "resBoost": emyStats[4] += emySkills["resBoost"]
+            x.setStats(emyStats)
 
-    def getCurrentTiles(self):
-        return self.currentTiles
+    def getMap(self): return self.map
 
-
+    # get enemy units if boolean true, player if false
     def getNextUnits(self,boolean):
         if boolean: return self.enemyUnits
         else: return self.playerUnits
 
-
+# one current state, with a set map
 class State:
-    def __init__(self,prev,map,phase,leftToAct):
+    def __init__(self,prev,map,phase,leftToAct,turn):
         self.prev = prev
-        self.map = map
+        self.map = copy(map)
         self.phase = phase # false - player, true - enemy
         self.leftToAct = leftToAct[:]
+        self.turn = turn
 
-    def doMove(self,unit,newTile):
+
+        #atkStats[1] += attacker.getWeapon().getMT()
+        #defStats[1] += defender.getWeapon().getMT()
+
+    # ifIsStartOfTurn
+    # distribute bonuses, lasts until start of next turn (or if removed?)
+    # distribute penalties, lasts until unit moves, or if removed
+    # distribute effects
+
+    def doMove(self,newTile,unit):
         oldTile = unit.getTile()
         oldTile.setHero(None)
         unit.setTile(newTile)
         newTile.setHero(unit)
 
         i = 0
-
         while i < len(self.leftToAct):
             if self.leftToAct[i] == unit:
                 self.leftToAct.pop(i)
             i += 1
         if len(self.leftToAct) == 0:
-            next = State(self,self.map,not self.phase,self.map.getNextUnits(not self.phase))
+            add = 0
+            if self.phase == True: add = 1
+            next = State(self,self.map,not self.phase,self.map.getNextUnits(not self.phase),self.turn+add)
         else :
-            next = State(self,self.map,self.phase,self.leftToAct)
+            next = State(self,self.map,self.phase,self.leftToAct,self.turn)
 
+        return next
 
+    def getPhase(self):
+        return self.phase
+
+    def getLeftToAct(self):
+        return self.leftToAct
+
+    def getMap(self):
+        return self.map
+
+    def getTurn(self):
+        return self.turn
 
     def getPossibleMoves(self,unit):
         move = unit.getMovement()
@@ -227,8 +278,9 @@ class State:
         while i < len(tiles):
             if tiles[i].getHero() != None:
                 tiles.pop(i)
-                if i != 0: i -= 1
+                i -= 1
             i += 1
+        #print("NEXT")
         tiles.append(unit.getTile())
 
         # 3 possible moves for each unit at a given state
@@ -249,10 +301,49 @@ startingTilesPlayer = [(7,1),(7,2),(7,3),(7,4)]
 startingTilesEnemy = [(0,1),(0,2),(0,3),(0,4)]
 map = Map(startingTilesPlayer,startingTilesEnemy)
 map.placeUnits(playerUnits,enemyUnits)
+#print(map.getMap()[0][2].getHero().getStats())
 
-startState = State(-1,map,False,playerUnits)
-moves = startState.getPossibleMoves(ephraim)
-startState.doMove(ephraim,moves[0])
+def simulate(playerUnits,enemyUnits,map):
+    turn = 1
+    startState = State(-1, map, False, playerUnits, turn)
+    stack = []
+    stack.append(startState)
+
+    while stack:  # while stack has states
+        cur = stack.pop() # get top of stack
+        curRem = cur.getLeftToAct() # get units that can act at this state
+        i = len(curRem) - 1 # start at end of remaining units list
+        if i == -1: # enemy phase case
+            #print("AAAAA")
+            eMap = copy(cur.getMap())
+            eState = State(cur, eMap, True, enemyUnits, cur.getTurn)
+            eState.enemyPhase()
+            pMap = copy(eState.getMap())
+            pState = State(eState, pMap, False, playerUnits, pState.getTurn() + 1)
+        else:
+            while i >= 0: # loops through deleting each unit in rem units list
+                tempRem = curRem[:] # create copy of remaining units
+                r = tempRem.pop(i) # this guy sucks
+                for curUnit in tempRem: # everyone else,
+                    moves = cur.getPossibleMoves(curUnit) # get what they can do wait what am i cooking
+                    for m in moves: # for each of that unit's moves
+                        newMap = copy(cur.getMap()) # copy map
+                        #if len(tempRem) < 1: print("hiiiiiiiiii")
+                        newState = State(cur, newMap, False, tempRem, cur.getTurn) # make state
+                        newState.doMove(m, curRem[i]) # do the move the current unit is doing
+                        stack.append(newState)  # add modified state to stack
+                if len(tempRem) == 0:
+                    rMoves = cur.getPossibleMoves(r)
+                    for m in rMoves:
+                        newMap = copy(cur.getMap())
+                        newState = State(cur, newMap, False, tempRem, cur.getTurn)
+                        newState.doMove(m, curRem[i])
+                i -= 1
+
+
+    return False
+
+simulate(playerUnits,enemyUnits,map)
 
 daMap = map.getMap()
 
@@ -262,16 +353,33 @@ for x in daMap:
         else: print(None,end='')
         #print(y.getCoords()," ",end='')
     print()
-
-
-
 print()
-#for x in daMap:
-#    for y in x:
-#
-#        if y.getEast() != None: print(y.getEast().getCoords()," ",end='')
-#    print()
 
-print(daMap[0][2].getTilesWithinNSpaces(2))
-print(daMap[0][2].isWithinNTiles(nowi,2))
-# current issue - control which move is next
+def printMoves(moves):
+    for x in moves:
+        print(x.getX(),x.getY())
+
+startState = State(-1,map,False,playerUnits,1)
+print("Ephraim,Nino,Takumi,Marth")
+
+#moves = startState.getPossibleMoves(ephraim)
+#printMoves(moves)
+#print()
+#nextState = startState.doMove(ephraim,moves[2])
+#moves = nextState.getPossibleMoves(nino)
+#printMoves(moves)
+#print()
+#nextNextState = nextState.doMove(nino,moves[3])
+#moves = nextNextState.getPossibleMoves(takumi)
+#printMoves(moves)
+#print()
+#nextNextNextState = nextNextState.doMove(takumi,moves[3])
+#moves = nextNextNextState.getPossibleMoves(marth)
+#printMoves(moves)
+#print()
+#finalState = nextNextNextState.doMove(marth,moves[4])
+
+#print(finalState.getPhase())
+#print(finalState.getLeftToAct()[0].getName())
+
+
